486a8aa174f3beca40aa9ec5ec6b36843f8a9a2c
lyn-eva <zeiyarlynn@gmail.com>
2022-11-06 21:53:14 +0630

    added zod for schema validation

--- src/api/routes/index.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from 'express';
-
-const version = '/api/v1';
-
-module.exports = (app: Express) => {
-  app.use(version + '/item', require('./item.routes'));
-  app.use(version + '/user', require('./user.routes'));
-  app.use(version + '/transaction', require('./transaction.routes'));
-};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import { Express } from 'express';
+import type { Express } from 'express';
 const express = require('express');
 
 const app: Express = express();
 
-require('./api/routes/index.routes')(app); // routes
+app.use(express.json()); // parse JSON body
+
+require('./api/routes/index.route')(app); // routes
 
 app.listen(process.env.PORT ?? 5000);
